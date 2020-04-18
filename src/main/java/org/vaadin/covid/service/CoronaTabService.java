f0ea96dec3543f87d7ca3caa6eb6fcdcdae2c187
Alejandro Duarte <alejandro.d.a@gmail.com>
2020-04-18 13:40:32 -0500

    Adds production profile

--- src/main/java/org/vaadin/covid/service/CoronaTabService.java
+++ src/main/java/org/vaadin/covid/service/CoronaTabService.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(name="corona-tab", url="https://api.coronatab.app")
+@FeignClient(name = "corona-tab", url = "https://api.coronatab.app")
 public interface CoronaTabService {
 
     @RequestMapping("/places")
