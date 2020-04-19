527e511cc5d8e0565f39a503f6ebbb364e049c43
Alejandro Duarte <alejandro.d.a@gmail.com>
2020-04-18 19:35:21 -0500

    Adds cache

--- src/main/java/org/vaadin/covid/Application.java
+++ src/main/java/org/vaadin/covid/Application.java
@@ -2,14 +2,24 @@ package org.vaadin.covid;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableCaching
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CacheManager cacheManager() {
+        return new ConcurrentMapCacheManager("covid");
+    }
 
 }
--- src/main/java/org/vaadin/covid/service/CoronaTabService.java
+++ src/main/java/org/vaadin/covid/service/CoronaTabService.java
@@ -1,5 +1,6 @@
 package org.vaadin.covid.service;
 
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -8,12 +9,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @FeignClient(name = "corona-tab", url = "https://api.coronatab.app")
 public interface CoronaTabService {
 
+    @Cacheable(cacheNames = "covid")
     @RequestMapping(value = "/places", headers = "User-Agent=Mozilla/5.0")
     DataWrapper<Place> findAllPlaces();
 
+    @Cacheable(cacheNames = "covid")
     @RequestMapping(value="/places/closest", headers = "User-Agent=Mozilla/5.0")
     DataWrapper<Place> getClosest(@RequestHeader("X-Forwarded-For") String ip);
 
+    @Cacheable(cacheNames = "covid")
     @RequestMapping(value="/places/{placeId}/data", headers = "User-Agent=Mozilla/5.0")
     DataWrapper<Stats> getStats(@PathVariable String placeId);
 
